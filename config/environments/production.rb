3be4094162a3bd38f26af70c89459b73976d5595
Reinhardt Lin <reinhardt.lin@gmail.com>
2014-12-05 12:17:27 +0800

    Add email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "http://beta.globaljobscope.com" }
+  config.action_mailer.smtp_settings = YAML.load( File.read("#{Rails.root}/config/email.yml") ).symbolize_keys
+
+
 end
